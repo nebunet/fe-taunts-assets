24b4be289ff8fd9371f548a8059d6a885187895d
nebunet <109931844+nebunet@users.noreply.github.com>
2022-10-22 19:04:20 -0400

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,27 @@
+if syn then
+local function mkfile(file, content, dir)
+    if not isfile(file) then
+        if dir ~= false then
+            writefile(dir.."/"..file, content)
+        else
+            writefile(file, content)
+        end
+    end
+end
+
+local function mkfolder(name, dir)
+    if not isfolder(name) then
+        if dir ~= false then
+            makefolder(dir.."/"..name)
+        else
+            makefolder(name)
+        end
+    end
+end
+
+mkfolder("FE-Taunts-Assets", false)
+mkfile("party.mp3", game:HttpGet("https://github.com/nebunet/fe-taunts-assets/blob/main/we_like_to_party.mp3?raw=true"), "FE-Taunts-Assets")
+
+else
+    print("It seems like you aren't using synapse. Music will not be heard for this script...")
+end
